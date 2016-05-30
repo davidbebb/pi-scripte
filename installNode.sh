064367d5bc5aeeb677ea4991dc1c09268eb61643
David Bebb <davidbebb@gmail.com>
2016-05-30 12:53:36 +0100

    updated install node tocopy to global and remove temp files

--- installNode.sh
+++ installNode.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#Get latest node 4
 wget https://nodejs.org/dist/v4.4.5/node-v4.4.5-linux-armv7l.tar.gz
 tar -xvf node-v4.4.5-linux-armv7l.tar.gz
+
+#Move to /usr/local
+cd node-v4.4.5-linux-armv7l
+sudo cp -R * /usr/local/
+
+
+#remove temp files
+rm -rf node-v4.4.5-linux-armv7l*
