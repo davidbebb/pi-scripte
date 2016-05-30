68a1c00368890120f430a911f17d06524c9af57b
David Bebb <davidbebb@gmail.com>
2016-05-30 13:11:23 +0100

    updated kweb install to get dependencies as well

--- kwebInstall.sh
+++ kwebInstall.sh
@@ -6,6 +6,7 @@ tar -xzf kweb-1.7.1.tar.gz
 cd kweb-1.7.1
 ./debinstall
 
-
+# Install dependencies
+sudo apt-get install xterm uget evince lxterminal tint2
 
 echo 'manual at : http://steinerdatenbank.de/software/kweb_manual.pdf'
